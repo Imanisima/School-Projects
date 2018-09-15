d3762d4f5d814a9cfd2eb959aac4719fb6319fa1
Imani A <22506762+Imanisima@users.noreply.github.com>
2018-09-15 17:44:05 -0600

    Complete
    Passwords are successfully generated!
    Passwords are saved in an external file along with respective users
    Passwords list is created in order to prevent loss of passwords
    Program now prints both username and associated passwords and hashes

--- BruteForceAndPwCracking/UltraHackz.py
+++ BruteForceAndPwCracking/UltraHackz.py
@@ -1,9 +1,6 @@
 import hashlib
-import string
-import random
 
 """
-Name: Imani Martin
 Lab: 1: Recursion
 Date: 4.September.2018
 Class: CS2302 - 1:30
@@ -12,7 +9,6 @@ Purpose: Using recursion, generate all possible passwords.
 
 About: A txt file is uploaded containing 100 records with information on 100 system accounts. For each record, there is a username, salt value, and hashed password. Generate an equivalent hash to the real account's password.
 
-Comments: Passwords successfully generated recursively. Drawback: Because there are so many passwords and accounts, it takes over 5 minutes to generate.
 
 """
 
@@ -50,47 +46,45 @@ def readFromTxt(username, saltValue, originalHash):
     f.close()
 
 
-# Generate a list of words
+# Sends values to be compared, and returns password if found to below method
 def generatePossiblePw2(username, minChars, saltValue, originalHash):
+    passwordFound = ""
+    for j in range(len(originalHash)):
+        for i in range((len(originalHash))**2):
+            i = str(i).format(i).zfill(minChars)
+            passwordFound = compareHash(i, saltValue[j], originalHash[j], username[j])
+
+    return passwordFound
+
+# Recursively calls on the above method to generate passwords, and saves them to a list
+def generatePossiblePw(username, minChars, maxChars, saltValue, originalHash):
     passwords = []
 
-    print("genPw2 Method")
-    minChars = 3
-    for i in range(1000):
-        print("i is now: ", i)
-        for j in range(len(originalHash)):
-            i = str(i).format(i).zfill(minChars) # acheives the 000 - 9999chars
-            print("salt value: ", saltValue[j]) # testing
-            password = compareHash(i, saltValue[j], originalHash[j])
-            passwords.append(password) # testing
-            print(passwords) # testing
-    return passwords
-
-
-def generatePossiblePw(username, minChars, maxChars, saltValue, originalHash):
-    print("genpw method")
     if minChars < maxChars:
-        password = generatePossiblePw2(username, minChars+1, saltValue, originalHash)
-        return password + generatePossiblePw(username, minChars+1, maxChars, saltValue, originalHash)
+        password = generatePossiblePw2(username, minChars, saltValue, originalHash)
+        passwords.append(password)
+        return passwords + generatePossiblePw(username, minChars+1, maxChars, saltValue, originalHash)
     else:
         return []
 
 
-# Comparing hash from the records to password generated
-def compareHash(password, saltValue, originalHash):
-    print("Compare Hash Method")
+# Comparing hash from the records to the password generated
+def compareHash(password, saltValue, originalHash, username):
     for i in range(len(originalHash)):
-        print("Original Hash: ", originalHash) # testing
-        newStr = password + saltValue  # concating password and salt value
-        print("newStr: ", newStr) # testing
-        newHash = hash_with_sha256(newStr)  # hashing the new pw
-        print("newHash: ", newHash) # testing
+        # Concating salt value and password to create new hash
+        newStr = password + saltValue
+        newHash = hash_with_sha256(newStr)
 
-        if newHash == originalHash:  # password equivalent found
-            print("Password found!")
+        # password equivalent found
+        if newHash == originalHash:
+            # Save new password to a file
+            with open("found_Pws.txt", "a+") as pwList:
+                pwList.write(username + ": " + password + "\n")
+            # pwList.close()
+
+            print(username + "\n Original Hash: %s\n Compared Hash: %s\n Equivalent password: %s" % (originalHash, newHash, password))
             return password
 
-        print("Password not found")
         return -1
 
 
