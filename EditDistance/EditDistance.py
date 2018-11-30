363b4be1336150c890a0b2f11a8fb70ae70e8114
Imani A <22506762+Imanisima@users.noreply.github.com>
2018-11-30 14:19:53 -0700

    Aesthetics and user input
    Example of edit distance is first displayed before user is asked if they'd like to input their own strings. If not, they have a choice to display the example once again.

--- EditDistance/EditDistance.py
+++ EditDistance/EditDistance.py
@@ -1,3 +1,7 @@
+import sys
+import time
+import subprocess
+import os
 import numpy as np
 
 """
@@ -7,6 +11,20 @@ Class: 2302 1:30
 
 Purpose: Implement Edit Distance algorithm
 """
+
+
+def display_example():
+    print("-" * 35)
+    print("\t\t~`~\tEXAMPLE\t~`~")
+    print("-" * 35)
+
+    s1 = "money"
+    s2 = "miners"
+
+    print("str1: %s\n"
+          "str2: %s" % (s1, s2))
+
+    print("\nEdit Distance: %d" % (edit_distance(s1, s2)))
 
 
 def edit_distance(s1, s2):
@@ -42,18 +60,42 @@ def edit_distance(s1, s2):
                                   m[i-1][j-1])  # replace
 
     print("\nPrinting matrix: \n", np.array(m))
+
     return m[len_s1][len_s2]
 
 
 def main():
-    s1 = "money"
-    s2 = "miners"
+    print("EDIT DISTANCE")
+    """ ----------------------------
+    |           Example            |
+    -------------------------------- """
+    display_example()
+    time.sleep(4)
 
-    print("Comparing the following strings: \n"
-          "str1: %s\n"
-          "str2: %s" % (s1, s2))
+    """ ---------------------------- 
+    |           User Input         |
+    -------------------------------- """
+    print("-" * 35)
+    print("\t\t~`~\tUSER INPUT\t~`~")
+    print("-"*35)
 
-    print("\nEdit Distance: %d" % (edit_distance(s1, s2)))
+    yes = 'Y'.lower()
+    while yes == 'Y'.lower():
+
+        user_choice = input("Would you like to compare your own pair of strings? [y/n]")
+        if user_choice != yes:
+            user_choice2 = input("Would you like to look at the example again?[y/n]")
+            if user_choice2 == 'Y'.lower():
+                display_example()
+                time.sleep(2)
+                sys.exit()
+            sys.exit()
+        else:
+            s1 = input("-> Please type in a string to compare: ")
+            s2 = input("-> Please type in another string to compare: ")
+
+            print("s1: %s\ns2: %s" % (s1, s2))
+            print("Edit Distance: %d" % (edit_distance(s1, s2)))
 
 
 if __name__ == '__main__':
