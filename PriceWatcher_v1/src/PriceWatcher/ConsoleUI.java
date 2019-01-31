f568d25662675b523021ff35b006f86370bcdd16
Imani A <22506762+Imanisima@users.noreply.github.com>
2019-01-30 22:22:37 -0700

    Hard-coded information needed for Price Watcher fields Included user input for switch case Created methods necessary for retrieving info for Item class

--- PriceWatcher_v1/src/PriceWatcher/ConsoleUI.java
+++ PriceWatcher_v1/src/PriceWatcher/ConsoleUI.java
@@ -1,31 +1,40 @@
 package PriceWatcher;
 import java.util.Scanner;
 
-public class ConsoleUI {
+class ConsoleUI {
     private Item item;
     Scanner userChoice = new Scanner(System.in);
+
+    static final int CHECKPRICE = 1;
+
+    static final int VIEWPAGE = 2;
+
+    static final int QUITAPP = -1;
 
     public ConsoleUI(Item item) {
         this.item = item;
     }
 
     public void showWelcome() {
-        System.out.println("Welcome to Price Watcher!");
+        System.out.println("WELCOME TO Price Watcher!");
     }
 
+    /* Used for testing purposes! Need to avoid magic numbers */
     public void showItem() {
-        System.out.println("\nItem details...");
+        System.out.println();
+
+        Item.getName("Google - Home Hub");
+        Item.getUrl("https://www.bestbuy.com/site/google-home-hub-with-google-assistant-chalk/6290313.p?skuId=6290313");
+        Item.getPrice(99.99);
+        Item.getPriceChange(0.00); // (a - b) / a * 100
+        Item.getDateAdded("01/30/2019  ($99.99)");
     }
 
-    public static final int checkPrice = 1;
-
-    public static final int viewPage = 2;
-
-    public static final int quitApp = -1;
 
     public int promptUser() {
+        System.out.println();
 
-        System.out.printf("Please choose an option. Enter: \n %d : Check Price\n %d : View Page\n%d : Quit", checkPrice, viewPage, quitApp);
+        System.out.printf("Please choose an option. Enter: \n %d : Check Price\n %d : View Page\n%d : Quit", CHECKPRICE, VIEWPAGE, QUITAPP);
 
         System.out.println();
 
