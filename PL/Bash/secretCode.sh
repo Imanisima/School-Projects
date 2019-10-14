8177b8f65354a9f6e77b42950bcb9a9ff6c649f1
Imani A <imartin@miners.utep.edu>
2019-10-14 15:47:57 -0600

    Adding new files to keep track of sorting

--- PL/Bash/secretCode.sh
+++ PL/Bash/secretCode.sh
@@ -7,6 +7,7 @@ FISHLIST="mini-fishlist.txt"
 EDITED_FISHLIST="fishlist-with-spaces.txt"
 MINI_CODEBOOK="mini-codebook.txt"
 NEW_LIST="newList.txt"
+SORTED_LIST="sorted_list.txt"
 
 # prints out the hashed numbers in fish-list
 echo -e "fish list prints hashed numbers only" >> output.txt
@@ -20,12 +21,18 @@ awk '{print $1}' $FISHLIST >> output.txt
 echo -e "\nList fish whose chars are 4 characters and less" >> output.txt
 awk 'length($1) == 4 {print $2}' $FISHLIST | sed 's/-/ /g' >> $EDITED_FISHLIST # dashes have now been replaced with spaces
 
+# delete line if first half of SKU is even
 echo -e "\nIf number is even, delete the field from the list" >> output.txt
 awk '($1%2==1)' $EDITED_FISHLIST | tr -s ' ' | cut -d ' ' -f1,2 >> $NEW_LIST
 
 # sort second field in ascending order
 echo -e "\nSort first column in ascending order" >> output.txt
-sort -n -k 1 $NEW_LIST >> output.txt
+sort -n -k 1 $NEW_LIST >> $SORTED_LIST
+
+# Add three(3) to second half of SKU and then convert the new number to ASCII
+echo -e "\nAdd 3 to the end of each SKU"
+
+
 
 
 
